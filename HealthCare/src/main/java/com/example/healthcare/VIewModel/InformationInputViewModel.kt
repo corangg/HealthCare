68494c9c42b0a515520d3a21804b04801bf74e0d
kang <dlwprkdlq@naver.com>
2024-02-21 03:26:31 +0900

    - InformationInputActivity생성 슬라이더 작업 중

--- HealthCare/src/main/java/com/example/healthcare/VIewModel/InformationInputViewModel.kt
+++ HealthCare/src/main/java/com/example/healthcare/VIewModel/InformationInputViewModel.kt
@@ -2,9 +2,17 @@ package com.example.healthcare.VIewModel
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class InformationInputViewModel: ViewModel() {
-    var backgroundColor = mutableStateOf(Color(0xFF182120))
+
+
+    var backgroundColor = mutableStateOf(Color(0xFF121212))
         private set
+    val heightValue : MutableLiveData<Float> = MutableLiveData()
+    fun setHeightValue(value : Float){
+        val stringValue = String.format("%.1f",value)
+        heightValue.value = stringValue.toFloat()
+    }
 }
--- HealthCare/src/main/java/com/example/healthcare/ui/InformationInputActivity.kt
+++ HealthCare/src/main/java/com/example/healthcare/ui/InformationInputActivity.kt
@@ -1,27 +1,41 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.healthcare.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -30,13 +44,38 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.example.healthcare.VIewModel.InformationInputViewModel
-import com.example.healthcare.VIewModel.MainViewModel
 import com.example.healthcare.ViewModelFactory.ViewModelFactory
 import com.example.healthcare.ui.theme.HealthCareTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import com.example.healthcare.R
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.input.key.Key.Companion.K
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+
+
+
+
 
 class InformationInputActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -56,9 +95,25 @@ class InformationInputActivity : ComponentActivity() {
 
 @Composable
 fun InformationInputView(viewModel: InformationInputViewModel) {
-    var sliderValue by remember { mutableStateOf(0f) } // 슬라이더의 값 상태를 관리
-    val sliderValueText = sliderValue.toString() // 슬라이더 값의 문자열 표현
+    val sliderValue by viewModel.heightValue.observeAsState(0f)
+    val text = remember { mutableStateOf(sliderValue.toString()) }
+    val sliderMaxValue = 300f
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    val backgroundClickAction = Modifier.clickable {
+        focusManager.clearFocus()
+    }
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(backgroundClickAction) // 배경 클릭 액션을 적용합니다.
+            .background(MaterialTheme.colorScheme.background)
+    )
+
+    LaunchedEffect(sliderValue) {
+        text.value = String.format("%.1f", sliderValue)
+    }
 
     Column(
         modifier = Modifier
@@ -67,19 +122,75 @@ fun InformationInputView(viewModel: InformationInputViewModel) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("정보 입력", style = TextStyle(color = Color.White,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold))
+        Text("정보 입력", style = TextStyle(color = Color.White, fontSize = 20.sp, fontWeight = FontWeight.Bold))
+        Spacer(modifier = Modifier.height(8.dp))
 
-        Spacer(modifier = Modifier.height(8.dp)) // 텍스트와 슬라이더 사이의 여백
-        Slider(
-            value = sliderValue,
-            onValueChange = { sliderValue = it },
-            valueRange = 0f..100f // 슬라이더 값의 범위
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_height),
+                contentDescription = "Slider Image",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(border = BorderStroke(3.dp, Color.White), shape = CircleShape)
+                    .padding(6.dp),
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+            Slider(
+                value = sliderValue,
+                onValueChange = { value ->
+                    viewModel.setHeightValue(value)
+                },
+                valueRange = 0f..sliderMaxValue,
+                colors = SliderDefaults.colors(
+                    activeTrackColor = Color.White,
+                    inactiveTrackColor = Color.Gray,
+                    thumbColor = Color.White
+                ),
+                modifier = Modifier
+                    .width(160.dp)
+                    .padding(start = 10.dp)
+            )
+
+            BasicTextField(
+                value = text.value,
+                onValueChange = { newValue ->
+                    val floatValue = newValue.toFloatOrNull()
+                    if (floatValue == null) {
+                        if (newValue.isEmpty()) text.value = ""
+                    } else {
+                        if (floatValue <= sliderMaxValue) {
+                            text.value = String.format("%.1f", floatValue)
+                            viewModel.setHeightValue(floatValue)
+                        }
+                    }
+                },
+                textStyle = TextStyle(color = Color.White, fontSize = 16.sp),
+                singleLine = true,
+                cursorBrush = SolidColor(Color.White),
+                modifier = Modifier
+                    .background(Color(0xFF2D2D2D), RoundedCornerShape(8.dp))
+                    .padding(horizontal = 10.dp, vertical = 8.dp)
+                    .width(42.dp)
+                    .focusRequester(focusRequester)
+                    .onFocusChanged { focusState ->
+                        if (focusState.isFocused && text.value == "0.0") {
+                            text.value = ""
+                        } else if (!focusState.isFocused && text.value.isEmpty()) {
+                            text.value = "0.0"
+                        }
+                                    },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+            )
+            Text(
+                text = "cm",
+                modifier = Modifier.padding(start = 10.dp),
+                color = Color.White,
+                fontSize = 22.sp
+            )
+        }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
